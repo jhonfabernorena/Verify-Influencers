ac7aae86c3914f0b260b19cd55fe5af97fbc4a8c
jhonfabernorena <91093100+jhonfabernorena@users.noreply.github.com>
2025-01-07 18:05:27 -0500

    Add-swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Verify Influencers API')
+    .setDescription('API para la verificación de influencers de salud')
+    .setVersion('1.0')
+    .addTag('influencers')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Aquí definimos el endpoint "/api" para ver Swagger
+  console.log('Swagger on http://localhost:3000/api#/');
+  
+
   await app.listen(3000);
 }
 bootstrap();
+
+
